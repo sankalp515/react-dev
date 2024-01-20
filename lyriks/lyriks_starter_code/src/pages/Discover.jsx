aeab3389cc6957ad8e8fb0d9fae7445cf4e3a84b
sankalp515 <157201316+sankalp515@users.noreply.github.com>
2024-01-20 16:58:08 +0000

    lyriks

---
+++ lyriks/lyriks_starter_code/src/pages/Discover.jsx
@@ -0,0 +1,15 @@
+import {Error,Loader,SongCard} from '../components';
+import {genres} from '../assets/constants'
+
+const Discover = () => {
+    return(
+        <div className='flex flex-col'>
+            <div className='w-full flex justify-between item-center sm:flex-row flex-col mt-4 mb-10'>
+                <h2></h2>
+
+            </div>
+        </div>
+    )
+};
+
+export default Discover;
